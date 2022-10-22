7e64ca3850498ed06c800101381bff4c59fac60b
sofia fraga <iq.sofiafraga@gmail.com>
2022-10-22 18:06:20 -0300

    Se actualizan los componentes Login, CrearCuenta y Buscador, y se incluyen los datos usuario y ciudades.

--- FrontEnd/src/components/Buscador.js
+++ FrontEnd/src/components/Buscador.js
@@ -3,21 +3,25 @@ import DatePicker, {registerLocale} from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import es from 'date-fns/locale/es'
 registerLocale("es",es)
-const Buscador = () =>{
-    
-    //se usa un array provisoriamente. Luego hay que usar JSON
-    const ciudades = ["Buenos Aires", "Mar del Plata", "Bariloche"]
+
+const Buscador = ({dataCiudades}) =>{
 
     const [ciudad, setCiudad] = useState('')
-    const [startDate, setStartDate] = useState(null);
-    const [endDate, setEndDate] = useState(null);
-    const [error,setError] = useState(false)
+    const [openCalendar, setOpenCalendar]= useState(false)
+    const [dateRange, setDateRange] = useState([null, null]);
+    const [startDate, endDate] = dateRange;
+    const[inputCalendar, setInputCalendar]= useState('Check in - Check out')
+
+    const months = ["ene.", "feb.", "mar.", "abr.", "may.", "jun.", "jul.", "ago.", "sep.", "oct.", "nov.", "dic."];
+    const formatoFecha = ()=>{
+    if (endDate === null) {setInputCalendar('Check in - Check out')} else{setInputCalendar(`${startDate.getDate()} de ${months[startDate.getMonth()]} - ${endDate.getDate()} de ${months[endDate.getMonth()]}`)}
+    }
 
     const handleSubmit = (event) =>{event.preventDefault()}
-    const handleOnChangeDate = (dates) => {
-        const [start, end] = dates;
-        setStartDate(start);
-        setEndDate(end);
+
+    const handleCalendar = (e) => {
+        e.preventDefault()
+        setOpenCalendar(true);
       };
 
     return(
@@ -26,21 +30,78 @@ const Buscador = () =>{
             <form onSubmit={handleSubmit}>
                 <select onChange={(e) =>{setCiudad(e.target.value)}} value={ciudad}>
                     <option value="default" hidden>¿A dónde vamos?</option>
-                    {ciudades.map((ciudad, index)=><option key={index}>{ciudad}</option>)}
+                    {dataCiudades.map((ciudad, index)=>
+                    <option key={index}>
+                        {`${ciudad.Ciudad} ${ciudad.Pais}`}
+                    </option>)}
                 </select>
+            <>
+                <button onClick={handleCalendar}>
+                {inputCalendar}
+                </button>
+                {openCalendar && (
                 <DatePicker 
+                renderCustomHeader={({
+                    monthDate,
+                    customHeaderCount,
+                    decreaseMonth,
+                    increaseMonth,
+                }) => (
+                    <div>
+                    <button
+                        aria-label="Previous Month"
+                        className={
+                        "react-datepicker__navigation react-datepicker__navigation--previous"
+                        }
+                        style={customHeaderCount === 1 ? { visibility: "hidden" } : null}
+                        onClick={decreaseMonth}
+                    >
+                        <span
+                        className={
+                            "react-datepicker__navigation-icon react-datepicker__navigation-icon--previous"
+                        }
+                        >
+                        {"<"}
+                        </span>
+                    </button>
+                    <span className="react-datepicker__current-month">
+                        {monthDate.toLocaleString("es", {
+                        month: "long",
+                        })}
+                    </span>
+                    <button
+                        aria-label="Next Month"
+                        className={
+                        "react-datepicker__navigation react-datepicker__navigation--next"
+                        }
+                        style={customHeaderCount === 0 ? { visibility: "hidden" } : null}
+                        onClick={increaseMonth}
+                    >
+                        <span
+                        className={
+                            "react-datepicker__navigation-icon react-datepicker__navigation-icon--next"
+                        }
+                        >
+                        {">"}
+                        </span>
+                    </button>
+                    </div>
+                )}
                     placeholderText='Check in - Check out'
-                    locale="es"
-                    dateFormat="dd/MM/yyyy" 
-                    selected={startDate} 
-                    OnChange= {handleOnChangeDate} 
-                    startDate={startDate} 
-                    endDate={endDate} 
-                    selectsRange 
-                    monthsShown={2}>
+                    inline 
+                    monthsShown={2}
+                    selectsRange={true}
+                    startDate={startDate}
+                    endDate={endDate}
+                    onChange={(update) => {
+                        setDateRange(update)
+                    }}
+                    >
                     <br></br>
-                    <button onClick={()=>{}}>Aplicar</button>
+                    <button onClick={(e) => {setOpenCalendar(false); formatoFecha()}}>Aplicar</button>
                 </DatePicker>
+                )}
+                </>
                 <button type="submit">Buscar</button>
             </form>
         </div>
---
+++ FrontEnd/src/data/usuario.js
@@ -0,0 +1,5 @@
+export const usuarioPrueba ={
+    'Email': "name@DH.com", 
+    'Password': "123456"
+}
+export const getdatos = () => usuarioPrueba
