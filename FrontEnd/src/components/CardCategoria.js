8581ea27727c3599ab9d3b3f60b8c602c1ef3add
cartunduaga06 <cartunduaga@gmail.com>
2022-10-24 18:10:23 -0500

    card categorias actualizado

--- FrontEnd/src/components/CardCategoria.js
+++ FrontEnd/src/components/CardCategoria.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const CardCategoria = ({ title, img }) => {
+const CardCategoria = ({ title, img, info }) => {
   return (
     <>
       <div className="card-categoria">
         <img className="img-categoria" src={img} alt="img" />
         <h2>{title}</h2>
+        <span className="card-categoria-span"> {info}</span>
       </div>
     </>
   );
--- FrontEnd/src/data/categorias.js
+++ FrontEnd/src/data/categorias.js
@@ -3,21 +3,25 @@ export const categorias = [
     {
         "Crimg": "https://images.unsplash.com/photo-1592965046687-1acdbcdb5642?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80",
         "Category": "Hoteles",
+        "info": "807.105 hoteles"
         
     },
     {
         "Crimg": "https://images.unsplash.com/photo-1592965046687-1acdbcdb5642?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80",
         "Category": "Hoteles",
+        "info": "807.105 hoteles"
         
     },
     {
         "Crimg": "https://images.unsplash.com/photo-1592965046687-1acdbcdb5642?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80",
         "Category": "Hoteles",
+        "info": "807.105 hoteles"
         
     },
     {
         "Crimg": "https://images.unsplash.com/photo-1592965046687-1acdbcdb5642?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80",
         "Category": "Hoteles",
+        "info": "807.105 hoteles"
         
     }
     
