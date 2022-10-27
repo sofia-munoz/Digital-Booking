489e8b69fe1a07b5ed42e0d8bd64c638a8609b8b
cartunduaga06 <cartunduaga@gmail.com>
2022-10-26 23:05:55 -0500

    actualizo json

--- FrontEnd/src/data/categorias.js
+++ FrontEnd/src/data/categorias.js
@@ -7,19 +7,19 @@ export const categorias = [
         
     },
     {
-        "Crimg": "https://images.unsplash.com/photo-1592965046687-1acdbcdb5642?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80",
+        "Crimg": "https://images.unsplash.com/photo-1601701119533-fde20cecbf4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
         "Category": "Hoteles",
         "info": "807.105 hoteles"
         
     },
     {
-        "Crimg": "https://images.unsplash.com/photo-1592965046687-1acdbcdb5642?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80",
+        "Crimg": "https://images.unsplash.com/photo-1615880484746-a134be9a6ecf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
         "Category": "Hoteles",
         "info": "807.105 hoteles"
         
     },
     {
-        "Crimg": "https://images.unsplash.com/photo-1592965046687-1acdbcdb5642?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1633&q=80",
+        "Crimg": "https://images.unsplash.com/photo-1650928367430-254e3e672dd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
         "Category": "Hoteles",
         "info": "807.105 hoteles"
         
