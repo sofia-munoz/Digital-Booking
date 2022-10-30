2aac7774b281ccd97989fa1dca2b7ac080c0f656
cartunduaga06 <cartunduaga@gmail.com>
2022-10-30 13:21:34 -0500

    actualizo cards

--- FrontEnd/src/components/Card.js
+++ FrontEnd/src/components/Card.js
@@ -1,24 +1,54 @@
-import React from 'react'
-import { AiFillStar } from "react-icons/fa";
+import React from "react";
+
 import { GrStar } from "react-icons/gr";
-import "..//styles/card.css"
-const Card = ({img, categoria, nombre, ubicacion, descripcion}) => {
+import "..//styles/card.css";
+import { HiLocationMarker, HiWifi } from "react-icons/hi";
+import { MdPool } from "react-icons/md";
+
+const Card = ({ img, categoria, nombre, ubicacion, descripcion }) => {
   return (
     <div className="card">
-    <div className='card-image'>
-        <img  className="img-mediana" src={img} alt="img" />
-    </div>
-    <div className="card-body">
-        <span className='font-categoria'>{categoria} <GrStar  className='start' /><GrStar  className='start' /><GrStar  className='start' /><GrStar  className='start' /><GrStar  className='start' /><GrStar  className='start' /> </span>
-        <span className="calificacion"> 8 </span>
-        <h3>{nombre}</h3>
-        <h4>{ubicacion}</h4>
-        <p>{descripcion} <span className='card-span'> más...</span></p>
+      <div className="card-image">
+        <img className="img-mediana" src={img} alt="img" />
+      </div>
+      <div className="card-body">
+        <div className="card-title">
+          <div>
+            <span className="font-categoria">
+              {categoria} <GrStar className="start" />
+              <GrStar className="start" />
+              <GrStar className="start" />
+              <GrStar className="start" />
+              <GrStar className="start" />{" "}
+            </span>
+            <h3>{nombre}</h3>
+          </div>
+
+          <div className="card-calificacion">
+            <span className="calificacion"> 8 </span>
+            <span className=""> Muy bueno </span>
+          </div>
+        </div>
+
+        <h4>
+          {" "}
+          <HiLocationMarker /> {ubicacion}{" "}
+          <span className="card-span"> MOSTRAR EN EL MAPA</span>
+        </h4>
+
+        <span className="card-icono">
+          {" "}
+          <HiWifi /> <MdPool />
+        </span>
+
+        <p>
+          {descripcion} <span className="card-span"> más...</span>
+        </p>
+
         <button className="btn-primary">Ver más</button>
-
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
