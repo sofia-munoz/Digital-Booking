faedcc9786dc22df0dce69f5328e7d52a0f248d2
celfiew <celfiew@gmail.com>
2022-11-08 21:25:37 -0500

    inyeccion de datos

--- BackEnd/proyecto-integrador/demo/src/main/java/com/example/demo/proyecto/model/Producto.java
+++ BackEnd/proyecto-integrador/demo/src/main/java/com/example/demo/proyecto/model/Producto.java
@@ -15,6 +15,7 @@ public class Producto {
     private Integer id;
     private String titulo;
     private String tituloDescripcion;
+    @Column(length=500)
     private String descripcion;
     private String disponibilidad;
     private String imagenPrincipalURL;
