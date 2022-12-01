63b2a59c79e0c29e43098996fe9cfdef780c688b
Cesar <celfiew@gmail.com>
2022-11-30 19:34:36 -0500

    resolving constructor issuee

--- BackEnd/proyecto-integrador/demo/src/main/java/com/example/demo/proyecto/model/Reserva.java
+++ BackEnd/proyecto-integrador/demo/src/main/java/com/example/demo/proyecto/model/Reserva.java
@@ -37,62 +37,13 @@ public class Reserva {
     @JoinColumn(name = "id_usuario", nullable = false)
     private Usuario usuario;
 
-    public Reserva() {
-    }
-    public Reserva(Integer id, LocalDate fechaInicial, LocalDate fechaFinal, LocalTime horaLLegada, Producto producto, Usuario usuario) {
-        this.id = id;
+
+    public Reserva(LocalDate fechaInicial, LocalDate fechaFinal, LocalTime horaLLegada, Producto producto, Usuario usuario) {
+
         this.fechaInicial = fechaInicial;
         this.fechaFinal = fechaFinal;
         HoraLLegada = horaLLegada;
         this.producto = producto;
-        this.usuario = usuario;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public LocalDate getFechaInicial() {
-        return fechaInicial;
-    }
-
-    public void setFechaInicial(LocalDate fechaInicial) {
-        this.fechaInicial = fechaInicial;
-    }
-
-    public LocalDate getFechaFinal() {
-        return fechaFinal;
-    }
-
-    public void setFechaFinal(LocalDate fechaFinal) {
-        this.fechaFinal = fechaFinal;
-    }
-
-    public LocalTime getHoraLLegada() {
-        return HoraLLegada;
-    }
-
-    public void setHoraLLegada(LocalTime horaLLegada) {
-        HoraLLegada = horaLLegada;
-    }
-
-    public Producto getProducto() {
-        return producto;
-    }
-
-    public void setProducto(Producto producto) {
-        this.producto = producto;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
 }
