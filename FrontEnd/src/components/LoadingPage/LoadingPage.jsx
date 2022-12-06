40d8c6bd586b6deb051c7d1ea6e1338e0d3810d4
Cesar <celfiew@gmail.com>
2022-12-06 18:27:06 -0500

    only development

--- FrontEnd/src/components/LoadingPage/LoadingPage.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './loading.module.css';
-
-export default function LoadingPage () {
-    return(
-        <div className={styles.loading_container}>
-            <div className={styles.loader}></div>
-            <div className={styles.loading_text}>Loading... </div>   
-        </div>
-    )}
