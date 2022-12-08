cd6e3df4f6ca9491493482fa593cc28a1c098b9a
KarinaScartissue <karinadominguez87@gmail.com>
2022-12-07 22:11:10 -0300

    ultimas modificaciones en la rama

--- BackEnd/proyecto-integrador/demo/src/main/java/com/example/demo/proyecto/repository/CaracteristicaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.proyecto.repository;
-
-import com.example.demo.proyecto.model.Caracteristica;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CaracteristicaRepository extends JpaRepository<Caracteristica,Integer> {
-}
