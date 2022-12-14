5bbf23da9c1493dd1166f7b4a4e9187bdcdd48df
Hector Hostos <leocomco@yahoo.es>
2022-12-14 02:16:41 +0000

    Old content deleted

--- BackEnd/proyecto-integrador/demo/src/main/java/com/example/demo/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class BadRequestException extends Exception {
-    public BadRequestException(String message){
-        super(message);
-    }
-
-}
--- BackEnd/proyecto-integrador/demo/src/main/java/com/example/demo/exception/ReferentialIntegrityException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exception;
-
-public class ReferentialIntegrityException extends Exception{
-    public ReferentialIntegrityException(String message){
-        super(message);
-    }
-}
-
